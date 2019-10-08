23c2e909b385996344306311a27b12e86aa004aa
Chris Maki <chris.maki@ripcitysoftware.com>
2019-10-08 05:20:55 -0700

    Remove JPA Entities and liquibase submodule

--- src/main/java/com/ripcitysoftware/productservice/ProductController.java
+++ src/main/java/com/ripcitysoftware/productservice/ProductController.java
@@ -1,94 +1,40 @@
 package com.ripcitysoftware.productservice;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.*;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
 @Slf4j
 public class ProductController {
 
-    private final ProductRepository productRepository;
-
-    public ProductController(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
-
     @GetMapping("/products/{id}")
     @CrossOrigin
     @ResponseBody
     public Optional<Product> getProduct(@PathVariable("id") Long id) {
         log.info("/products/{} invoked!", id);
-        return productRepository.findById(id);
+        return Optional.of(Product.builder()
+                .id(1L)
+                .companyName("sample company")
+                .gtin("0123456789012")
+                .labelDescription("KitKat is a perfect balance of chocolate and wafer first launched in the UK")
+                .build());
     }
 }
-
-@Repository
-@RepositoryRestResource
-interface ProductRepository extends JpaRepository<Product, Long> {}
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @ToString
-@Entity
-@JsonIgnoreProperties
 class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "product_id")
     private Long id;
     private String gtin;
-    @Column(name = "company_name")
     private String companyName;
-    @Column(name = "label_description")
     private String labelDescription;
-    @Column(name = "medium_image_url")
     private String mediumImageUrl;
 }
 
-@Entity
-class Person {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
-    private String firstName;
-    private String lastName;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-}
-
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
-interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
-
-    List<Person> findByLastName(@Param("name") String name);
-
-}
--- src/main/liquibase/sql/db.initial-data.sql
+++
@@ -1,3 +0,0 @@
-insert into product (PRODUCT_ID, GTIN, COMPANY_NAME, BRAND, LABEL_DESCRIPTION, MEDIUM_IMAGE_URL, VENDOR,
-                     LANGUAGE_CODE, DATE_REGISTERED, DATE_UPDATED)
-values (1, '0123456789012', 'Nestle', 'KitKat', 'KitKat is a perfect balance of chocolate and wafer first launched in the UK in 1935 as ''Chocolate Crisp''. The KitKat brand name and logo was introduced on pack in 1937.', null, 'Marx Bros.', 'EN', '2019-07-01 12:22:00.00', '2019-07-01 12:22:00.00');
