55ecfe5dfd8fea85726184905e03a6d8112cb3d1
Chris Maki <chris.maki@ripcitysoftware.com>
2019-10-08 05:29:33 -0700

    add StreamLambdaHandler to project

--- src/main/java/com/ripcitysoftware/productservice/ProductServivce.java
+++ src/main/java/com/ripcitysoftware/productservice/ProductServivce.java
@@ -2,8 +2,10 @@ package com.ripcitysoftware.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({ProductController.class})
 public class ProductServivce {
 
 	public static void main(String[] args) {
